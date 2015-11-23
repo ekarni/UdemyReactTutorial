c04f66580ab3b3221c147a202fa0ab61fe71e33b
Elad Karni <ekarni@problemsolutions.net>
2015-11-23 15:39:01 -0800

    used .gitignore to ignore node_module folder

--- thumbnail-gulp/gulpfile.js
+++ thumbnail-gulp/gulpfile.js
@@ -5,25 +5,25 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
 
- gulp.task('default', function() {
-	 var bundler = watchify(browserify({
-		 entries: ['./src/app.jsx'],
-		 transform:[reactify],
-		 extentions:['.jsx'],
-		 debug: true,
-		 cache: {},
-		 packageCache: {},
-		 fullPath: true
-	 }));
+gulp.task('default', function() {
+ var bundler = watchify(browserify({
+	 entries: ['./src/app.jsx'],
+	 transform:[reactify],
+	 extentions:['.jsx'],
+	 debug: true,
+	 cache: {},
+	 packageCache: {},
+	 fullPath: true
+ }));
 
-	 function build(file) {
-		 if (file) gutil.log('Recompiling ' + file)
-		 return bundler
-		 	.bundler()
-			.on('error', gutil.log.bind(gutil, 'Browserify Error'))
-			.pipe(source('main.js'))
-			.pipe(gulp.dest('./'));
-	 };
-	 build()
-	 bundler.on('update', build)
- });
+ function build(file) {
+	 if (file) gutil.log('Recompiling ' + file)
+	 return bundler
+	 	.bundler()
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('./'));
+ };
+ build()
+ bundler.on('update', build)
+});

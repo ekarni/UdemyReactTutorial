ff728670d8025278293caafa8e1daa20972b0a84
Elad Karni <ekarni@problemsolutions.net>
2015-11-25 14:07:38 -0800

    End of Video 1 - Made a simple dropdown button

--- dropdown/src/app.jsx
+++ dropdown/src/app.jsx
@@ -1,25 +1,19 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
+var ReactDOM = require('react-dom');
+var Dropdown = require('./dropdown');
 
 //Main options for the app. Kept in one location for easy access
 var options = {
-	thumbnailData: [{
-		title: 'Show Courses',
-		number: 32,
-		header: 'Learn React',
-		description: 'React is a fantastic new library for making fast, dynamic pages.',
-		imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
-	},{
-		title: 'Show Courses',
-		number: 25,
-		header: 'Learn Gulp',
-		description: 'Gulp will speed up your development worflow a lot. Gulp will speed up your development worflow a lot.',
-		imageUrl: 'http://brunch.io/images/others/gulp.png'
-	}]
+	title: 'Choose a dessert',
+	items: [
+		'Apple Pie',
+		'Peach Cobbler',
+		'Coconut Cream Pie'
+	]
 };
 
 //Creating an instance of the Class (Cookie)
-var element = React.createElement(ThumbnailList, options);
+var element = React.createElement(Dropdown, options);
 
 //Displaying the instance (Eating the cookie?!)
-React.render(element, document.querySelector('.container'));
+ReactDOM.render(element, document.querySelector('.container'));
---
+++ dropdown/src/dropdown.jsx
@@ -0,0 +1,15 @@
+// We need to show a button and a list
+//This component should know when to show the list based on the users interaction
+
+var React = require('react');
+var Button = require('./button');
+//var List = require('./list');
+
+module.exports = React.createClass ({
+  render: function() {
+    return <div className="dropdown">
+      <Button className="btn-default" title={this.props.title} subTitleClassName="caret" />
+    </div>
+  }
+
+});

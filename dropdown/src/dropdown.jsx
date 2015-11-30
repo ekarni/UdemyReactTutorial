aa2b56e87bea14e9efea343fec2654100ba0ebc3
Elad Karni <ekarni@problemsolutions.net>
2015-11-30 12:12:13 -0800

    Chapter 4 Lectrue 20

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -3,12 +3,26 @@
 
 var React = require('react');
 var Button = require('./button');
-//var List = require('./list');
-
+var ListItem = require('./list-item');
 module.exports = React.createClass ({
+  handleClick: function() {
+    alert('Hello from dropdown');
+  },
   render: function() {
+    var list = this.props.items.map(function(item){
+      return <ListItem item={item} />
+    });
+
     return <div className="dropdown">
-      <Button className="btn-default" title={this.props.title} subTitleClassName="caret" />
+      <Button
+        whenClicked={this.handleClick}
+        className="btn-default"
+        title={this.props.title}
+        subTitleClassName="caret"
+      />
+    <ul>
+      {list}
+    </ul>
     </div>
   }
 
